7afe0b895c22880dec19047546aaecf96e15fed5
douzone-bipa <douzone.bipa@gmail.com>
2021-09-07 09:59:47 +0900

    first commit

---
+++ practice01/src/main/java/prob1/Prob1.java
@@ -0,0 +1,14 @@
+package prob1;
+
+import java.util.Scanner;
+
+public class Prob1 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner( System.in );
+		
+		/* 코드 작성 */
+		System.out.println(".....");
+		
+		scanner.close();
+	}
+}
