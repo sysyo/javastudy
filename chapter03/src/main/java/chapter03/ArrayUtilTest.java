1656373a2f5663ddb96735c4b6bbae7eee0c2737
sysyo <youngiya77@gmail.com>
2021-09-28 19:20:17 +0900

    testttttt 0720

--- chapter03/src/main/java/chapter03/ArrayUtilTest.java
+++ chapter03/src/main/java/chapter03/ArrayUtilTest.java
@@ -5,8 +5,7 @@ import java.util.Arrays;
 public class ArrayUtilTest {
 
 	public static void main(String[] args) {
-//		double[] d1 = ArrayUtilTest.intToDouble(new int[]{10, 20, 30, 40, 50});
-//		System.out.println(Arrays.toString(d1)); 
+
 //
 //		int[] a2 = ArrayUtil.doubleToInt(new double[]{10.1, 20.2, 30.3, 40.4, 50.5});
 //		System.out.println(Arrays.toString(a2));
