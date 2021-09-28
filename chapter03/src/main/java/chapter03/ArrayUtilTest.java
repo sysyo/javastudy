eb92a4151a1764ec2e9e2dab0f317f7896921b96
sysyo <youngiya77@gmail.com>
2021-09-28 19:16:22 +0900

    test heo

--- chapter03/src/main/java/chapter03/ArrayUtilTest.java
+++ chapter03/src/main/java/chapter03/ArrayUtilTest.java
@@ -5,14 +5,14 @@ import java.util.Arrays;
 public class ArrayUtilTest {
 
 	public static void main(String[] args) {
-		double[] d1 = ArrayUtilTest.intToDouble(new int[]{10, 20, 30, 40, 50});
-		System.out.println(Arrays.toString(d1)); 
-
-		int[] a2 = ArrayUtil.doubleToInt(new double[]{10.1, 20.2, 30.3, 40.4, 50.5});
-		System.out.println(Arrays.toString(a2));
-		
-		int[] a3 = ArrayUtil.concat(new int[]{1, 2, 3, 4}, new int[]{5, 6, 7, 8});
-		System.out.println(Arrays.toString(a3));
+//		double[] d1 = ArrayUtilTest.intToDouble(new int[]{10, 20, 30, 40, 50});
+//		System.out.println(Arrays.toString(d1)); 
+//
+//		int[] a2 = ArrayUtil.doubleToInt(new double[]{10.1, 20.2, 30.3, 40.4, 50.5});
+//		System.out.println(Arrays.toString(a2));
+//		
+//		int[] a3 = ArrayUtil.concat(new int[]{1, 2, 3, 4}, new int[]{5, 6, 7, 8});
+//		System.out.println(Arrays.toString(a3));
 		
 	}
 
